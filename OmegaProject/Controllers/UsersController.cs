33c2501e697565434a03e209e4254bbb36c1481a
momotan <momotaha039@gmail.com>
2022-05-15 03:32:27 +0300

    'put'

--- OmegaProject/Controllers/UsersController.cs
+++ OmegaProject/Controllers/UsersController.cs
@@ -23,8 +23,11 @@ namespace OmegaProject.Controllers
         public IActionResult GetUsers(int id=-1)
         {
             if(id==-1)
-            return Ok(db.Users);
-
+            {
+                var users = db.Users.ToList();
+                users.Reverse();
+                return Ok(users);
+            }
             UserDTO user=db.Users.FirstOrDefault(x => x.Id==id);
             if (user == null)
                 return BadRequest("Not found User");
@@ -32,6 +35,51 @@ namespace OmegaProject.Controllers
             return Ok(user);
         }
 
+
+        [HttpPost]
+        [Route("PostUser")]
+        public IActionResult GetUsers([FromBody] UserDTO user)
+        {
+            var temp=db.Users.FirstOrDefault(x => x.IdCard==user.IdCard);
+            if(temp!=null)
+                return BadRequest("Faild Added ...This User Exist !!");
+            db.Users.Add(user);
+            db.SaveChanges();
+            return Ok("Added Successfully");
+        }
+
+        [HttpDelete]
+        [Route("DeleteUser/{id}")]
+        public IActionResult DeleteUser(string id)
+        {
+            //check if user Existed
+            var temp = db.Users.FirstOrDefault(x => x.IdCard ==id);
+            if (temp == null)
+                return BadRequest("Faild Deleted ...This User not Exist !!");
+
+            db.Users.Remove(temp);
+            db.SaveChanges();
+            return Ok("Deleted Successfully");
+        }
+
+        [HttpPut]
+        [Route("EditUser")]
+        public IActionResult EditUser([FromBody] UserDTO user)
+        {
+            //check if user Existed
+            var temp = db.Users.FirstOrDefault(x => x.Id == user.Id);
+            if (temp == null)
+                return BadRequest("Faild Editing ...This User not Exist !!");
+            temp.Role = user.Role;
+            temp.FirstName = user.FirstName;
+            temp.LastName = user.LastName;
+            temp.Email = user.Email;
+            temp.Password = user.Password;
+            temp.Phone = user.Phone;
+            temp.IdCard = user.IdCard;
+            db.SaveChanges();
+            return Ok("Editing Successfully");
+        }
 
     }
 }
